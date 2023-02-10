de78a0fc3b7fdb2c80d34233b4ac54afdb9a4c7b
Julieta359 <119441514+Julieta359@users.noreply.github.com>
2023-02-10 14:41:12 -0300

    Deploy

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -7,7 +7,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://backend-srnf.onrender.com/personas/';
 
   constructor(private http: HttpClient) { }
 
